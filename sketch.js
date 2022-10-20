ff461631d4ad5211da4d92d00bcfd85b5d77ab81
Gustav Paragas <gep21b@fsu.edu>
2022-10-20 10:55:25 -0400

    shape updates

--- sketch.js
+++ sketch.js
@@ -1,7 +1,20 @@
+let w = 600
+let h = 600
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(w,h);
 }
 
 function draw() {
+  fill("yellow")
+  stroke("black")
   background(125);
+  beginShape(TRIANGLE_STRIP);
+  vertex(40, 80);
+  vertex(60, 20);
+  vertex(60, 75);
+  vertex(80, 20);
+  vertex(90, 75);
+  vertex(80, 20);
+  vertex(90, 75);
+  endShape();
 }
